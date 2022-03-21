f37fb282b5eeee318dc2837e0de3032eaf1a8c4e
Elvin Mark Munoz Vega <munoz.vega.em@gmail.com>
2022-03-21 16:06:50 +0900

    adding new models in conv

--- models/conv.py
+++ models/conv.py
@@ -1,41 +1,41 @@
+from ast import Str
 from layers import BasicResidualBlock1d, BasicResidualBlock2d, BatchNorm1d, BatchNorm2d, Conv1d, Conv2d, Flatten, Linear, MaxPool1d, MaxPool2d, ReLU, Sequential
 
 
-def create_conv1d_classifier(image_shape=1000, input_channels=1, num_classes=10):
+def create_conv1d_classifier(wav_length=1000, input_channels=1, num_classes=10):
     modules = []
     i = 4
-    while image_shape > 1:
+    while wav_length > 3:
         output_channels = 2**i
         modules.append(Sequential(
-            Conv1d(input_channels, output_channels, bias=False, padding=1),
+            Conv1d(input_channels, output_channels,
+                   bias=False, kernel_size=3, padding=1),
             BatchNorm1d(output_channels),
             ReLU(),
-            MaxPool1d()
+            MaxPool1d(kernel_size=4)
         ))
         input_channels = output_channels
         i += 1
-        image_shape = image_shape // 2
-    modules.append(Flatten((-1, image_shape * image_shape * output_channels)))
-    modules.append(Linear(image_shape * image_shape *
-                   output_channels, num_classes))
+        wav_length = wav_length // 4
+    modules.append(Flatten((-1, wav_length * output_channels)))
+    modules.append(Linear(wav_length * output_channels, num_classes))
     return Sequential(*modules)
 
 
-def create_residual1d_classifier(image_shape=28, input_channels=1, num_classes=10):
+def create_residual1d_classifier(wav_length=1000, input_channels=1, num_classes=10):
     modules = []
     i = 4
-    while image_shape > 1:
+    while wav_length > 3:
         output_channels = 2**i
         modules.append(Sequential(
             BasicResidualBlock1d(input_channels, output_channels),
-            MaxPool1d())
+            MaxPool1d(kernel_size=4))
         )
         input_channels = output_channels
         i += 1
-        image_shape = image_shape // 2
-    modules.append(Flatten((-1, image_shape * image_shape * output_channels)))
-    modules.append(Linear(image_shape * image_shape *
-                   output_channels, num_classes))
+        wav_length = wav_length // 4
+    modules.append(Flatten((-1, wav_length * output_channels)))
+    modules.append(Linear(wav_length * output_channels, num_classes))
     return Sequential(*modules)
 
 
